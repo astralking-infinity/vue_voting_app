4f61a8c8c90eb00abbee073e0238846cfa809196
astralking <mao.chiu.astral@gmail.com>
2018-11-15 22:43:54 +0800

    Updated PollList component

--- src/components/PollList.vue
+++ src/components/PollList.vue
@@ -1,9 +1,65 @@
 <template>
-  <h1>Poll List page</h1>
+  <div v-if="!isLoaded" class="text-center">
+    <div v-if="errors && ('detail' in errors)">
+      <div v-for="(error, idx) in errors.detail"
+          :key="idx">
+        {{ error }}
+      </div>
+    </div>
+    <span v-else>{{ placeholder }}
+      <font-awesome-icon icon="circle-notch"></font-awesome-icon>
+    </span>
+  </div>
+
+  <div class="row justify-content-center" v-else>
+    <div class="col-lg-8 col-md-10 col-sm-12">
+      <div class="list-group">
+        <router-link v-for="(poll, index) in polls"
+                     :to="{ name: 'poll-detail', params: { pollId: poll.id } }"
+                     :key="`polls-${index}`"
+                     class="list-group-item list-group-item-action list-group-item-primary text-center text-truncate">
+          {{ poll.question }}
+        </router-link>
+      </div>
+      <!-- Pagination area -->
+    </div>
+  </div>
 </template>
 
 <script>
+  import { endpoints } from './config.js'
+
   export default {
-    name: 'pollList'
+    name: 'pollList',
+    data() {
+      return {
+        polls: [],
+        isLoaded: false,
+        placeholder: 'Loading...',
+        errors: null
+      }
+    },
+    mounted() {
+      fetch(endpoints.poll_list())
+        .then(response => {
+          if (!response.ok) {
+            throw response.json();
+          }
+          return response.json();
+        })
+        .then(polls => {
+          this.polls = polls
+          this.isLoaded = true
+        })
+        .catch(errorData => {
+          if (errorData instanceof Promise) {
+            errorData.then(errors => this.errors = errors );
+          } else {
+            this.errors = {
+              detail: [errorData.message]
+            }
+          }
+        });
+    }
   }
 </script>
