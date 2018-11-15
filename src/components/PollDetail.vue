2456295f6a8fb92433269f68fa92b39b3b0c2e5c
astralking <mao.chiu.astral@gmail.com>
2018-11-15 17:58:16 +0800

    Added components

---
+++ src/components/PollDetail.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Poll Detail page for id {{ this.$route.params.pollId }}</h1>
+</template>
+
+<script>
+  export default {
+    name: 'pollDetail'
+  }
+</script>
---
+++ src/components/PollList.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Poll List page</h1>
+</template>
+
+<script>
+  export default {
+    name: 'pollList'
+  }
+</script>
