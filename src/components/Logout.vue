b03c500b992bcee7a38c4c5a8299a01864de8150
astralking <mao.chiu.astral@gmail.com>
2018-11-17 01:49:40 +0800

    Added Logout component

--- src/components/Logout.vue
+++ src/components/Logout.vue
@@ -1,3 +1,47 @@
 <template>
-  <div>This is a logout page</div>
+  <p class="text-center">
+    Logging out... <font-awesome-icon icon="circle-notch" />
+  </p>
 </template>
+
+<script>
+  import { endpoints } from './config.js'
+
+  export default {
+    name: 'logout',
+    props: {
+      isAuthenticated: Boolean,
+      token: String
+    },
+    mounted() {
+      const { isAuthenticated, token } = this
+      if (isAuthenticated) {
+        const conf = {
+          method: 'post',
+          headers: new Headers({
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${token}`
+          })
+        }
+        fetch(endpoints.logout(), conf)
+          .then(response => {
+            if (!response.ok) {
+              throw response.json()
+            }
+            return response.json()
+          })
+          .then(successData => {
+            this.$emit('loggedOutCallback')
+            this.$router.replace({ name: 'home' })
+          })
+          .catch(errorData => {
+            if (errorData instanceof Promise) {
+              errorData.then(errors => console.log(errors))
+            } else {
+              console.log(errorData)
+            }
+          })
+      }
+    }
+  }
+</script>
