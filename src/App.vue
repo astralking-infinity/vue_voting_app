b5a418fe98755461c6f983a56258a05a3e0c4e8c
astralking <mao.chiu.astral@gmail.com>
2018-11-17 02:12:34 +0800

    Updated

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,73 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <router-view @loggedInCallback="loggedIn"
+                 @loggedOutCallback="loggedOut"
+                 :isAuthenticated="isAuthenticated"
+                 :user=user
+                 :token=token>
+    </router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import { endpoints } from './components/config.js'
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        isAuthenticated: false,
+        user: null,
+        token: null
+      }
+    },
+    mounted() {
+      const auth  = localStorage.getItem('auth');
+      if (auth) {
+        const { token } = JSON.parse(auth);
+        this.loggedIn(token);
+      }
+    },
+    methods: {
+      loggedIn(token) {
+        const conf = {
+          method: 'get',
+          headers: new Headers({
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${token}`
+          })
+        }
+
+        fetch(endpoints.user(), conf)
+          .then(response => {
+            if (!response.ok) {
+              throw response.json()
+            }
+            return response.json()
+          })
+          .then(user => {
+            const auth = JSON.stringify({ token })
+            localStorage.setItem('auth', auth)
+            this.isAuthenticated = true
+            this.user = user
+            this.token = token
+          })
+          .catch(errorData => {
+            if (errorData instanceof Promise) {
+              errorData.then(errors => console.log(errors))
+            } else {
+              console.log(errorData.message)
+            }
+          })
+      },
+      loggedOut() {
+        localStorage.removeItem('auth')
+        this.isAuthenticated = false
+        this.user = null
+        this.token = null
+      }
+    }
+  }
 </script>
 
 <style src="./style.css"></style>
