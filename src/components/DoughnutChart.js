ac62ca7a6f60718c306427326d3917163a6366c7
astralking <mao.chiu.astral@gmail.com>
2018-11-17 21:31:35 +0800

    Added plugin to extended chart to display a text if empty votes

--- src/components/DoughnutChart.js
+++ src/components/DoughnutChart.js
@@ -20,6 +20,26 @@ export default {
     options: Object
   },
   mounted() {
+    this.addPlugin({
+      id: 'placeholder-text',
+      afterDraw: function (chart) {
+        var votes = chart.data.datasets[0].data
+
+        var nonzeroVotes = votes.filter(vote => vote > 0)
+        if (nonzeroVotes.length === 0) {
+          var ctx = chart.chart.ctx
+          var width = chart.chart.width
+          var height = chart.chart.height
+          chart.clear()
+
+          ctx.save()
+          ctx.textAlign = 'center'
+          ctx.fillStyle = '#000'
+          ctx.fillText('No data to display', width / 2, height / 2)
+          ctx.restore()
+        }
+      }
+    })
     this.renderChart(this.chartData, this.options || defaultOptions)
   }
 }
